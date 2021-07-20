a0225a53d77d99b2ae9cb741c53b54c27ddc7939
Jorge Lopez-Nava <jlopezn1@swarthmore.edu>
2021-07-19 22:15:17 -0400

    Preparing TSZ for Apertium

---
+++ corpus/README.md
@@ -0,0 +1,4 @@
+# tsz
+This is a repository of information for the Purepecha Language. The information is for our LING073 course and is not meant to be shared publicly yet.
+
+A lot of the corpus was obtained from a Twitter account, and translated from Purepecha to Spanish, and then Spanish to English.
--- tests/disabiguation-tests.sh
+++ tests/disabiguation-tests.sh
@@ -1,9 +1,11 @@
-CORPUS=/home/jlopezn1/ling073/ling073-tsz-corpus/tsz.corpus.basic.txt
+# This is a bash script for automatically calculating ambiguity in our transducer
+CORPUS=/home/jlopezn1/ling073/ling073-tsz-spa-corpus/tsz.sentences.txt
 MORPH=/home/jlopezn1/ling073/ling073-tsz/tsz.automorf.bin
 RLX=/home/jlopezn1/ling073/ling073-tsz/tsz.rlx.bin # you might need to remove ".bin"
 TOKENS=`cat ${CORPUS} | apertium-destxt | lt-proc ${MORPH} | apertium-cleanstream | sed 's/$\W*\^/$\n^/g' | wc -l`
 echo $TOKENS
 ANALYSES=`cat ${CORPUS} | apertium-destxt | lt-proc ${MORPH} | apertium-cleanstream | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
+echo $ANALYSES
 DISAMB=`cat ${CORPUS} | apertium-destxt | lt-proc ${MORPH} | apertium-cleanstream | cg-proc ${RLX} | sed 's/$\W*\^/$\n^/g' | cut -f2- -d'/' | sed 's/\//\n/g' | wc -l`
 AMBIGPRE=`calc $ANALYSES/$TOKENS`
 AMBIGPOST=`calc $DISAMB/$TOKENS`
